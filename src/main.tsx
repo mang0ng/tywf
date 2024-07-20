55384d3c61951b1e3669b657c79fe1cab4ce68b7
mang0ng <mangogong5@gmail.com>
2024-07-19 23:34:19 -0700

    remove basename bug

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
     <React.StrictMode>
-        <HashRouter basename="/tywf">
+        <HashRouter>
             <App />
         </HashRouter>
     </React.StrictMode>
